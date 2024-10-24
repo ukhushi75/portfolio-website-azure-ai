0b7ec267fb89004176919c14a9001f9fc92087a9
Khushi Upadhyay <kupadhyay@etcc.com>
2024-10-24 16:53:00 -0500

    My first Commit

---
+++ app/api/route.js
@@ -0,0 +1,38 @@
+import {openAIClient, AzureKeyCredential } from '@azure/openai';
+import { NextResponse } from 'next/server'; 
+
+const endpoint = process.env.AZURE_OPENAI_ENDPOINT;
+const apiKey = process.env.AZURE_OPENAI_API_KEY;
+const model = process.env.AZURE_OPENAI_MODE;
+
+export async function POST(req){
+	
+	const { messages } = await req.json();
+
+	const client = new OpenAIClient(endpoint, new AzureKeyCredential(apiKey));
+
+	messages.unshift({
+		role: 'system',
+		content: `You are PortfolioGPT, answering only questions based on the resume provided.
+Resume:
+${DATA_RESUME}
+
+Help users learn more about Khushi from her resume.`
+	})
+    // Add Resume
+    // Setup proper System Messages
+
+    const response = await client.getChatCompletions(model, messages, {
+        maxTockens: 128,
+
+    })
+
+    return NextResponse.json({
+        message: response.choices[0].message.content
+    })
+}
+
+const DATA_RESUME = 
+' Hobbies: Khushi loves to gym and build websites like this one. Khushi Upadhyay (647) 544-2234| ukhushi75@gmail.com | LinkedIn |  GitHub EXECUTIVE SUMMARY A proficient computer engineering graduate, adept in software design, development, and testing. With exceptional analytical, technical, and programming skills, aspiring to excel as a computer engineer, driving innovation and achieving excellence in the field. EDUCATION  Bachelor of Computer Engineering (Specialized Honours)                                                                                                              Sep 19 – Apr 24                     Certificate in Technology Entrepreneurship (BEST)                                                                                                                York University, Toronto, Canada (Lassonde School of Engineering)  TECHNICAL SKILLS  Programming Languages:  Java, Spring, Python, Verilog, MATLAB, C++. Database Management Systems:  SQL, MYSQL. Other Tools/Libraries: Visual Studio Code, Jira, Tableau, REST API, Jupyter, Arduino, Eclipse, Quartus, WordPress, Git, GitHub, Google Collab, Embedded Systems. EXPERIENCE Student Success Mentor Lead (Excel Lassonde)           Jun 23 – Apr 24  Managed a free tutoring service under student welcome and support center at Lassonde School of Engineering, York University.  Lead a team of 30, resulting in a 30% increase in participation. Managed the financials of the service by creating comprehensive reports and proposals.  FrontEnd Developer at YuRide      Sep 22 – Dec 22  Utilized Java and Android Studio to develop an Android application, focusing on front-end development within an Agile environment.  Enhanced app performance by 15% through code minification and resolved issues with browser developer tools.  Achieved a 20% reduction in page load speeds and improved conversion rates by implementing ReactJS. Collaborated on application design and code using Miro, Figma, Microsoft Teams, GitHub, and Git. Tech Specialist at GAOTek Inc.    May 22 – Aug 22 Collaborated on the flow management of IEP for facsimile modulations in embedded development tools.   Wrote test cases and debugged ITU-T T.38 codes using embedded C on Visual Studio Code.  ACADEMIC PROJECTS AIAA Design, Build, and Fly                                                                                                                                                        Sep 23 - Present  Leading wireless communication as a part of the capstone project to build an RF 5ft airplane with real-time telemetry and autonomous flight capabilities. Collaborating on flight direction using Raspberry Pi and Python to meet AIAA competition requirements. Contributing to designing an aircraft that completes Mission 3 of the competition AIAA “Urban Taxi Flight” involving a five-minute window for takeoff, landing, and navigating a predetermined course with crew and passengers.  Warehouse Management System       Sep 23 - Dec 23 Created a warehouse management system prototype with client side and server-side UI with URL parameters.  Designed a UI software prototype of a warehouse management system with 6 use cases including login server, client server, placing an order, restocking, and many more. Applied more than more than 4 software design patterns. Traffic Light Signal     Sep 22 – Dec 22 Created traffic light signal prototype with the help of DE10-Lite and linked the board to the Verilog IoT Cloud and managed complex research equipment with two motors.   Used sequential and combinational logic in Verilog to make streetlight change red/green with the help of four ultrasonic sensors.  AWARDS / CERTIFICATIONS • UNHACK 2023 mentor certificate of appreciation • Voluntary contribution award at the 2018-2019 Lassonde Awards Ceremony • Associate Java Certification 1Z0-808 • 1Z0-819 - Java SE 11 Developer (In progress)'
+
+
---
+++ app/page.js
@@ -0,0 +1,337 @@
+    "use client";
+    import { useState } from "react";
+    import Image from "next/image";
+
+    export default function Home() {
+
+        const [menuOpen, setMenuOpen] = useState(false);
+        const [ messageInput, setMessageInput ] = useState('');
+
+        const [messages, setMessages] = useState([
+
+            {
+                role: 'assistant',
+                content: 'How can I help you learn more about Khushi and her Resume?'
+            }
+        ])
+        const submitForm = async (e) => {
+            e.preventDefault();
+            let newMessages = [...messages, { role: 'user', content: messageInput }];
+            setMessages(newMessages);
+            setMessageInput('');
+            const response = await fetch('/api', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ messages: newMessages })
+            }
+        ).then(res => res.json())
+        setMessages([...newMessages, { role: 'assistant', content: apiMessage.message }]);
+        }
+
+        
+
+        const toggleMobileMenu = () => {
+        setMenuOpen(!menuOpen);
+
+        }
+
+        return (
+        <>
+            <header>
+            <a href="#" className="logo-holder">
+                <div className="logo">K</div>
+                <div className="logo-text">Portfolio Website</div>
+            </a> 
+            <nav>
+                <ul id="menu" className={menuOpen ? "active" : ""}>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#skills">Skills</a>
+                    </li>
+                    <li>
+                        <a href="#projects">Projects</a>
+                    </li>
+                    <li>
+                        <a href="mailto:ukhushi75@gmail.com" 
+                        className="button">Contact Me</a>
+                    </li>
+                </ul>
+                <a href="#" className="mobile-toggle" onClick={toggleMobileMenu}>
+                <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" strokeLinecap="round" strokeWidth="2" d="M5 7h14M5 12h14M5 17h10"/>
+                </svg>
+                </a>
+            </nav>
+        </header>
+        <main>
+            <section className="hero container">
+                <div className="hero-blue">
+                    <div>
+                        <h1><small>Hi I'm</small>
+                            Khushi Upadhyay
+                        </h1>
+                        <p>
+                            I'm a Computer Engineer. I work as Software QA Engineer based in Texas. I specialize in software development, creating projects that showcase my skills in HTML, CSS, JavaScript, and Azure.                         
+                            <span>I'm interested in AI topics which is why I also add things like ChatGPT into my project these days. </span>
+                        </p>
+                        <div className="call-to-action">
+                            <a href="./Khushi_Upadhyay_Resume.pdf" className="button black">
+                                View Resume
+                            </a>
+                            <a href="mailto:ukhushi75@gmail.com" className="button white">
+                                Contact Me
+                            </a>
+                        </div>
+                        <div className="social-links">
+                            <a href="https://github.com/ukhushi75">
+                                <img src="./imgs/github.png" alt="GitHub" width="48"/>
+                            </a>
+                            <a href="https://www.linkedin.com/in/khushi-upadhyay/">
+                                    <img src="./imgs/linkedin.png" alt="LinkdedIn" width="48"/>
+                            </a>
+                        </div>
+                    </div>
+
+
+                </div>
+                <div className="hero-yellow">
+                    <img src="./imgs/hero-image-me_processed.png" alt="Khushi Upadhyay" width="100%"/>
+                </div>
+            </section>
+            <section className="logos container">
+                <div className="marquee">
+                    <div className="track">
+                        <img src="./imgs/html.png"
+                        alt="HTML" width="128" />
+                        <img src="./imgs/css.png"
+                        alt="HTML" width="128" />
+                        <img src="./imgs/javascript.png"
+                        alt="HTML" width="128" />
+                        <img src="./imgs/sass.png"
+                        alt="HTML" width="128" />
+                        <img src="./imgs/react.png"
+                        alt="HTML" width="128" />
+                        <img src="./imgs/nextjs.png"
+                        alt="HTML" width="128" />
+                        <img src="./imgs/azure.png"
+                        alt="HTML" width="128" />
+                        <img src="./imgs/vscode.png"
+                        alt="HTML" width="128" />
+                        <img src="./imgs/html.png"
+                        alt="HTML" width="128" />
+                        <img src="./imgs/css.png"
+                        alt="HTML" width="128" />
+                        <img src="./imgs/javascript.png"
+                        alt="HTML" width="128" />
+                        <img src="./imgs/sass.png"
+                        alt="HTML" width="128" />
+                        <img src="./imgs/react.png"
+                        alt="HTML" width="128" />
+                        <img src="./imgs/nextjs.png"
+                        alt="HTML" width="128" />
+                        <img src="./imgs/azure.png"
+                        alt="HTML" width="128" />
+                        <img src="./imgs/vscode.png"
+                        alt="HTML" width="128" />
+                        <img src="./imgs/html.png"
+                        alt="HTML" width="128" />
+                        <img src="./imgs/css.png"
+                        alt="HTML" width="128" />
+                        <img src="./imgs/javascript.png"
+                        alt="HTML" width="128" />
+                        <img src="./imgs/sass.png"
+                        alt="HTML" width="128" />
+                        <img src="./imgs/react.png"
+                        alt="HTML" width="128" />
+                        <img src="./imgs/nextjs.png"
+                        alt="HTML" width="128" />
+                        <img src="./imgs/azure.png"
+                        alt="HTML" width="128" />
+                        <img src="./imgs/vscode.png"
+                        alt="HTML" width="128" />
+
+                    </div>
+                </div>
+
+
+            </section>
+            <section id="skills" className="skills container">
+                <h2>
+                    <small>About Me</small>
+                    Skills
+                </h2>
+                <div className="holder-blue">
+                    <div className="left-column">
+                        <h3>Frontend</h3>
+                        <ul>
+                            <li>HTML</li>
+                            <li>CSS</li>
+                            <li>JavaScript</li>
+                            <li>React</li>
+                            <li>Angular</li>
+                            <li>Vue</li>
+                        </ul>
+                        <h3>Backend</h3>
+                        <ul>
+                            <li>Node.js</li>
+                            <li>Express</li>
+                            <li>Payton</li>
+                            <li>Java</li>
+                            <li>PHP</li>
+                            <li>Ruby</li>
+                        </ul>
+
+                    </div>
+                    <div className="right-column">
+                        <h3>A bit about me</h3>
+                        <p>Hi I'm Khushi Upadhyay, I'm a Computer Engineering graduate and Software QA Engineer 
+                            in Texas. I specialize in software development, 
+                            creating projects that showcase my skills in HTML, CSS, JavaScript, and Azure. 
+                            I'm interested in AI topics which is why I also add things like 
+                            ChatGPT into my project these days!
+                        </p>
+                        <p>
+                            I'm currenlty working on a project that uses Azure AI to create a chatbot 
+                            that can help answer questions about web developemnt. I am also working on a project that
+                            uses React and Next.js to create a Portfolio website.
+                        </p>
+                    </div>
+                </div>
+
+            </section>
+            <section className="work-experience container">
+                <h2>
+                    <small>Recent</small>
+                    Work Experience
+                </h2>
+                <div className="jobs">
+                    <article>
+                        <figure>
+                            <div>
+                                <img src="./imgs/work-experience-1.png" 
+                                alt="Workplace 1 - Software QA Engineer" 
+                                width="100%"/>
+                                <figcaption>
+                                    Software QA Engineer
+                                </figcaption>
+                            </div>
+                        </figure>
+                        <h3>Quarterhill</h3>
+                        <div>Jul 2024 - present</div>
+                        <p>A leading provider of tolling and enforceent solutions in the Intelligent Transportation System(ITS) industry.</p>
+                    </article>
+                    <article>
+                        <figure>
+                            <div>
+                                <img src="./imgs/work experince - 2.png" 
+                                alt="Workplace 2 - Frontend Developer" 
+                                width="100%"/>
+                                <figcaption>
+                                    Frontend Developer
+                                </figcaption>
+                            </div>
+                        </figure>
+                        <h3>YuRideshare</h3>
+                        <div>Sep 22 - Dec 22
+                        </div>
+                        <p>A startup collaborating with York University to offer a tailored, sustainable, and safe rideshare system.</p>
+                    </article>                
+                    <article>
+                        <figure>
+                            <div>
+                                <img src="./imgs/work experience -3.png" 
+                                alt="Workplace 3 - Tech Specialist" 
+                                width="100%"/>
+                                <figcaption>
+                                    Tech Specialist
+                                </figcaption>
+                            </div>
+                        </figure>
+                        <h3>GAO Tek</h3>
+                        <div>May 22 - Aug 22</div>
+                        <p>GAO Tek Inc., a member of GAO Group, is a global leader in providing test equipment, meters, fiber optic testers, drones, and more.</p>
+                    </article>
+                </div>
+            </section>
+            <section id="projects" className="bento container">
+                <h2>
+                    <small>
+                        Previous
+                    </small>
+                    Completed Projects
+                </h2>
+                <div className="bento-grid">
+                    <a href="https://lassonde.yorku.ca/lassonde-clubs" className="bento-item">
+                        <img src="./imgs/Project - 1.png" 
+                        alt="YorkU Excel Lassonde Club" 
+                        width="100%"/>
+                    </a>
+                    <a href="#" className="bento-item">
+                        <img src="./imgs/Project-2.png" 
+                        alt="AIAA RC Plane" 
+                        width="100%"/>
+                    </a>
+                    <a href="#" className="bento-item">
+                        <img src="./imgs/Project - 3.gif" 
+                        alt="ACTC Roadside - Quarterhill" 
+                        width="100%"/>
+                    </a>
+                    <a href="#" className="bento-item">
+                        <img src="./imgs/Project - 4.png" 
+                        alt="Foosball Game" 
+                        width="100%"/>
+                    </a>
+                    <a href="#" className="bento-item">
+                        <img src="./imgs/Project - 5.png" 
+                        alt="Portfolio website" 
+                        width="100%"/>
+                    </a>
+                    <a href="#" className="bento-item">
+                        <img src="./imgs/project - 6.png" 
+                        alt="Preprocessing and Topic Modeling Simple Crypto Widget" 
+                        width="100%"/>
+                    </a>
+                </div>
+            </section>
+            <section className="chatbot container">
+                <h2>
+                    <small>
+                        Talk to me
+                    </small>
+                    Chatbot
+                </h2>
+                <div className="chatbot-blue">
+                    <div className="chatbot-info">
+                        <h3>Azure AI Chatbot</h3>
+                        <p>I've put together a chatbot here which knows all my skills, work experience and has a copy of my CU/Reume.
+                            you can use it to ask questions about me to get a better idea of who I am and what I've done.</p>
+                        <p>You can also download my resume here if you want to take a look at it. I'm currently looking for new opportunites so if you have a project you think I'd be a good fit for, please get in touch!</p>
+                        <a href="./Khushi_Upadhyay_Resume.pdf" className="button black">Download Resume</a>
+                        <div className="chat-box">
+                            <div className="scroll-area">
+                                        <ul id="chat-log">
+                                {messages.map((message, index) => (
+                                    <li key={index} className={`${message.role}`}>
+                                    <span className={`avatar`}>{message.role === 'user' ? 'You' : 'AI'}</span>
+                                    <div className="message">{message.content}</div>
+                                    </li>
+                                ))}
+                                </ul>
+                            </div>
+                            <form onSubmit={submitForm} className="chat-message">
+                                <input type="text" 
+                                placeholder="Hey Khushi, what skills are you best at?" value={messageInput} onChange={e => setMessageInput(e.target.value)}/>
+                                <button className="button black">Send</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
+        </>
+        );
+    }
